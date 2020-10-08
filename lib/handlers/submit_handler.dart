5ecd8cdecce6f35a6bfe68a27cd12615cf1bdb5c
NCelliers <nico.celliers@sybrin.com>
2020-10-08 20:08:47 +0200

    Added Submit Handler and JSON

--- lib/handlers/submit_handler.dart
+++ lib/handlers/submit_handler.dart
@@ -1,4 +1,27 @@
+import 'package:access/models/data_result.dart';
+import 'package:access/models/form_model.dart';
+import 'package:flutter/cupertino.dart';
+
+import 'http_call_handler.dart';
+
 class SubmitHandler{
   
-  Future<DataResult<String>> submitData(String n)
+  Future<DataResult<String>> submitData(FormModel model) async {
+    DataResult<String> dataResult;
+    try {
+      DataResult<String> httpResult =
+          await HttpCallHandler.makeJsonCall(model.toJson(), model.url);
+      if (httpResult.success) {
+        dataResult = DataResult<String>(success: true, value: httpResult.value);
+      } else {
+        throw Exception(httpResult.error);
+      }
+    } catch (e) {
+      debugPrint('PROVIDER ERROR : $e');
+      dataResult =
+          DataResult<String>(success: false, error: "Error getting Hanis data");
+    }
+
+    return dataResult;
+  }
 }
--- lib/models/form_model.dart
+++ lib/models/form_model.dart
@@ -1,9 +1,22 @@
 import 'package:access/models/question_model.dart';
+import 'package:json_annotation/json_annotation.dart';
 
+part 'form_model.g.dart';
+
+@JsonSerializable()
 class FormModel {
+  final String url;
+  @JsonKey()
   final List<QuestionModel> answers;
+  @JsonKey()
   final DateTime startDate;
+  @JsonKey()
   final DateTime submitDate;
 
-  FormModel(this.answers, this.startDate, this.submitDate);
+  FormModel(this.url, this.answers, this.startDate, this.submitDate);
+
+  factory FormModel.fromJson(Map<String, dynamic> json) =>
+      _$FormModelFromJson(json);
+
+  Map<String, dynamic> toJson() => _$FormModelToJson(this); 
 }
